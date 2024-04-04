b9098b4656768d99eee3f1d6b55e987da307edc1
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-04 00:37:11 -0600

    refactored and linted post constant module

--- play.js
+++ play.js
@@ -1,10 +1,10 @@
 const connect = require('../snake-client/client');
-const setupInput = require('../snake-client/input')
+const setupInput = require('../snake-client/input');
 
+// function that connects keyboard inputs to server
 const dataPlusKeyboard = (connect) => {
   setupInput(connect);
-}
+};
+
 console.log("Connecting ...");
-
 dataPlusKeyboard(connect());
-
