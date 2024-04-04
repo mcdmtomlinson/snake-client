58e5557d582dfd00670be83aa33df8e8ffac5deb
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-04 00:26:33 -0600

    added WASD movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,8 +12,20 @@ const setupInput = function () {
 
 const handleUserInput = function (key) {
   if (key === '\u0003') { 
-    console.log("Disconnected from server 🙅")
+    console.log("End of session 🙅")
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
