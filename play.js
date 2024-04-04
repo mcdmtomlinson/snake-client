0cf77aea3b5f90ac972f0da80f2494afe06e3c6a
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-04 00:18:26 -0600

    added input module

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const connect = require('../snake-client/client');
+const setupInput = require('../snake-client/input')
 
 console.log("Connecting ...");
 
 connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    console.log("Disconnected from server 🙅")
-    process.exit();
-  }
-};
-
-setupInput();
