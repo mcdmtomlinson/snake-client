39d9e19f80e800bff35fbecd791cedccbca3ad5c
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-04 00:02:08 -0600

    added user inputs

--- play.js
+++ play.js
@@ -1,21 +1,40 @@
 const connect = require('./client');
 
-const connect = () => {
-	const conn = net.createConnection({
-		host: 'localhost',
-		port: 50541,
-	});
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = () => {
+	const stdin = process.stdin;
+	stdin.setRawMode(true);
+	stdin.setEncoding('utf8');
+	stdin.resume();
 
-  console.log('Connecting...');
+	stdin.on('data', handleUserInput);
+	return stdin;
+};
+console.log('Connecting...');
 
-	conn.setEncoding('utf8');
+connect();
+const handleUserInput = data => {
+	//if user presses d-pad it should send the command to move snake
+	if (data === 's') {
+		conn.write('Move: down');
+	} else if (data === 'w') {
+		conn.write('Move: up');
+	} else if (data === 'd') {
+		conn.write('Move: right');
+	} else if (data === 'a') {
+		conn.write('Move: left');
+	}
 
-	// detecting incoming data from the server then log the data
-	conn.on('data', data => {
-		console.log('you ded cuz you idled');
-	});
-	return conn;
+	// console.log(data);
+	//if user presses ctrl + c we do something
 };
 
-console.log('Connecting ...');
-connect();
+const conn = connect();
+
+// connect();
+setupInput();
+
+// d3/snake/snake-client
