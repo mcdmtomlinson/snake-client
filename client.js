28a63115e8ced4f300e60011c2351e20e3b117ab
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-04 00:33:49 -0600

    client.js

--- client.js
+++ client.js
@@ -1,31 +1,29 @@
 const net = require("net");
-const { setInterval } = require("timers/promises");
-const connect = function () {
+const { IP, PORT } = require("./constants");
+
+// establish ip and port
+const connect = function() {
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-    up: "Move: up",
-    down: "Move: down",
-    left: "Move: left",
-    right: "Move: right"
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  // set a message for connection 
+  // set a message for connection
   conn.on('connect', () => {
     console.log('Connected to the server! 🐍');
-    conn.write("Name: MCA");
-
+    conn.write("Name: EEM");
   });
-  // handle incoming data
+  // hanlde incoming data
   conn.on('data', (data) => {
     console.log(data.toString());
     conn.end();
   });
-  // set a message for disconnection 
+  // set a message for disconnection
   conn.on('end', () => {
     console.log('Disconnected from server 👋');
-  }); 
+  });
   return conn;
 };
+
 module.exports = connect;
