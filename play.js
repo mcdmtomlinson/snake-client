a3d39cb543ae712706623d4d15a021b6655eb869
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-04 00:13:00 -0600

    refactored and added disconnect message

--- play.js
+++ play.js
@@ -1,40 +1,23 @@
-const connect = require('./client');
+const connect = require('../snake-client/client');
 
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = () => {
-	const stdin = process.stdin;
-	stdin.setRawMode(true);
-	stdin.setEncoding('utf8');
-	stdin.resume();
-
-	stdin.on('data', handleUserInput);
-	return stdin;
-};
-console.log('Connecting...');
+console.log("Connecting ...");
 
 connect();
-const handleUserInput = data => {
-	//if user presses d-pad it should send the command to move snake
-	if (data === 's') {
-		conn.write('Move: down');
-	} else if (data === 'w') {
-		conn.write('Move: up');
-	} else if (data === 'd') {
-		conn.write('Move: right');
-	} else if (data === 'a') {
-		conn.write('Move: left');
-	}
 
-	// console.log(data);
-	//if user presses ctrl + c we do something
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
 };
 
-const conn = connect();
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    console.log("Disconnected from server 🙅")
+    process.exit();
+  }
+};
 
-// connect();
 setupInput();
-
-// d3/snake/snake-client
